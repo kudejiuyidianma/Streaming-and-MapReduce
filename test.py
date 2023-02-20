3793e16eebae0e4a93367d48c734ae2d27847c53
Yuqing <yuqing.wang.1@stonybrook.edu>
2023-02-20 00:22:38 -0500

    MapReduce

---
+++ test.py
@@ -0,0 +1,18 @@
+import sys
+from collections import deque
+from math import log, log2  # natural log
+from pprint import pprint
+from random import random
+from sys import getsizeof
+
+import mmh3  # hashing library
+import numpy as np
+
+# k = round(-log2(0.01) / log2(2))
+# seeds = [round(random()*100) for i in range(0,k)]
+# print(seeds)
+
+num = str(342)
+print(num[2:], '_', num[3:])
+print('sdfsdf')
+    
